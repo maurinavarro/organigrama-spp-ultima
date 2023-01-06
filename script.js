596c8728fa33aea2aa9a69e60838b3baeae587b9
maurinavarro <mauricionavarro28@gmail.com>
2023-01-06 09:23:44 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+if ($("#contenido").val() == "") {
+  $("#contenido").load("jerarquias/jefatura.html");
+}
+
+function jefatura() {
+  $("#contenido").empty();
+  $("#contenido").load("jerarquias/jefatura.html");
+}
+
+function direcciones_generales() {
+  $("#contenido").empty();
+  $("#contenido").load("jerarquias/direcciones_generales.html");
+}
+
+function direcciones() {
+  $("#contenido").empty();
+  $("#contenido").load("jerarquias/direcciones.html");
+}
+
+function departamentos() {
+  $("#contenido").empty();
+  $("#contenido").load("jerarquias/departamentos.html");
+}
+
+function divisiones() {
+  $("#contenido").empty();
+  $("#contenido").load("jerarquias/divisiones.html");
+}
+
+function secciones() {
+  $("#contenido").empty();
+  $("#contenido").load("jerarquias/secciones.html");
+}
